fceba5383d48b6dfb7badde3c509d709c0767c0b
MrDevloaper <MrDevloaper@gmail.com>
2025-02-13 18:11:46 +0530

    Update size ping.py

--- CRUSHMUSIC/plugins/tools/ping.py
+++ CRUSHMUSIC/plugins/tools/ping.py
@@ -17,7 +17,7 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://envs.sh/ysj.mp4",
+        video="https://envs.sh/EIH.mp4",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await PRASHANT.ping()
